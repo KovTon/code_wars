e9f2a5ddd19c70dc56706b14db4f82ccc1963502
KovTon <ikovtonogov@gmail.com>
2023-04-28 21:21:25 +0400

    Add module.

---
+++ cw_Bartender_drinks.py
@@ -0,0 +1,15 @@
+def get_drink_by_profession(param:str):
+    param = param.lower().title()
+    visitors = ["Jabroni", "School Counselor", "Programmer", "Bike Gang Member", "Politician", "Rapper"]
+    drinks = ["Patron Tequila", "Anything with Alcohol", "Hipster Craft Beer", "Moonshine", "Your tax dollars", "Cristal"]
+    visitors_drink_preferences = dict(zip(visitors, drinks))
+    drink = ''
+    try:
+        visitors_drink_preferences[param]
+        drink = visitors_drink_preferences.get(param)
+    except KeyError:
+        drink = "Beer"
+    return drink
+
+
+print(get_drink_by_profession('School Counselor'))
